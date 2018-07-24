81e05e1e032c41958d63bf08b661615854d80cae
CEIBA\david.jaramillo <david.blj95@gmail.com>
2018-07-24 07:47:23 -0500

    agregar pruebas en gradle

---
+++ src/main/java/com/david/greet/Greet.java
@@ -0,0 +1,14 @@
+package com.david.greet;
+
+public class Greet {
+	
+	public Greet() { }
+	
+	public String hello(String name) {
+		return "Hello " + name;
+	}
+  
+	public String bye(String name) {
+		return "Bye " + name;
+	}
+}
